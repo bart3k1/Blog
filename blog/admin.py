fc738e028aeeeed7c4cacc0f6a078f3af6888217
bart3k1 <bart3k1@gmail.com>
2019-06-21 17:53:33 +0200

    gitignore added

---
+++ blog/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from .models import Post, Comment
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish',
+                    'status', 'tag_list')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
+
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.tags.all())
+
+
+admin.site.register(Post, PostAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+
+
+admin.site.register(Comment, CommentAdmin)
